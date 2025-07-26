b8359110e80e8c82fd86a65e49248351ecb40634
Rima Chaib <102191962+rima-snyk@users.noreply.github.com>
2025-07-25 18:54:27 -0700

    Update userProfile.js

--- routes/userProfile.js
+++ routes/userProfile.js
@@ -51,17 +51,4 @@ module.exports = function getUserProfile () {
             'Content-Security-Policy': CSP
           })
 
-          res.send(fn(user.dataValues))
-        }).catch(error => {
-          next(error)
-        })
-      } else {
-        next(new Error('Blocked illegal activity by ' + req.connection.remoteAddress))
-      }
-    })
-  }
-
-  function favicon () {
-    return utils.extractFilename(config.get('application.favicon'))
-  }
-}
+          
